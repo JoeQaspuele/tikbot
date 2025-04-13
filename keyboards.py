c8b0aff30e0c255a1f14f6c96af963486babdf03
JoeQaspuele <112169301+JoeQaspuele@users.noreply.github.com>
2025-04-13 22:41:46 +0700

    Create keyboards.py

---
+++ keyboards.py
@@ -0,0 +1,21 @@
+# keyboards.py
+from telegram import ReplyKeyboardMarkup, KeyboardButton
+
+def main_menu_keyboard():
+    buttons = [
+        [KeyboardButton("📦 Заказать билет")],
+        [KeyboardButton("📄 Последний заказ"), KeyboardButton("📚 Все заказы")]
+    ]
+    return ReplyKeyboardMarkup(buttons, resize_keyboard=True)
+
+def registration_keyboard():
+    return ReplyKeyboardMarkup(
+        [[KeyboardButton("📝 Регистрация")]],
+        resize_keyboard=True
+    )
+
+def confirm_keyboard():
+    return ReplyKeyboardMarkup(
+        [[KeyboardButton("✅ Подтвердить"), KeyboardButton("❌ Отменить")]],
+        resize_keyboard=True
+    )
