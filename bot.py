de880cd42d8e78872f28cbdac3034ab472b81dc6
JoeQaspuele <112169301+JoeQaspuele@users.noreply.github.com>
2025-04-13 23:04:19 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,13 +4,16 @@ from settings import BOT_TOKEN
 from db import init_db
 from handlers.register import register_conv_handler
 from handlers.booking import booking_conv_handler  # ⬅️ добавляем импорт нового обработчика
+from handlers.register import start, register_conv_handler
+
 
 def main():
     init_db()
 
-    application = Application.builder().token(BOT_TOKEN).build()
-
     # Обработчики
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(register_conv_handler)
+    application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(register_conv_handler)   # регистрация
     application.add_handler(booking_conv_handler)    # заказ билета
 
