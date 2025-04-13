8823ecfa01985d9369292ab6676040526b27b6d0
JoeQaspuele <112169301+JoeQaspuele@users.noreply.github.com>
2025-04-13 22:40:08 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,13 @@ from telegram.ext import Application, CommandHandler, MessageHandler, filters, C
 from settings import BOT_TOKEN
 from handlers.register import register_conv_handler
 
+from db import init_db
+
+def main():
+    init_db()  # создаст таблицу users, если её нет
+    ...
+
+
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
 
